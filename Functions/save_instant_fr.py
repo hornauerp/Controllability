f54e5fed69f3dcedd3b9411adbc14f5319a47c57
phornauer <philipp.hornauer@bsse.ethz.ch>
2022-09-07 16:38:12 +0200

    Added folder structure and instantaneous firing rate function

---
+++ Functions/save_instant_fr.py
@@ -0,0 +1,23 @@
+import numpy as np
+from elephant.statistics import instantaneous_rate
+from elephant.kernels import GaussianKernel
+from neo.core.spiketrainlist import SpikeTrainList
+import quantities as pq
+import os
+
+
+def smooth_fr(input_path, output_path):
+    times_path = os.path.join(input_path, 'spike_times.npy')
+    ids_path = os.path.join(input_path, 'spike_templates.npy')
+    times = np.load(times_path)
+    ids = np.load(ids_path)
+    # get neotrains
+    neo_t: SpikeTrainList = SpikeTrainList.from_spike_time_array(times, ids, np.unique(ids), units='s',
+                                                                 t_stop=np.max(times))
+
+    # get smoothed data
+    smoothed = instantaneous_rate(neo_t, sampling_period=1 * pq.ms, kernel=GaussianKernel(3 * pq.ms))
+    np_array = np.array(smoothed).T
+    np.save(output_path, np_array)
+
+    return np_array
